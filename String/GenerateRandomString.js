4dc47391bf3a24d05c5843fa146bd870bf15c2e9
vaibhav <vaibhavshukla2811@gmail.com>
2024-05-19 12:05:05 +0530

    Generate Random String

---
+++ String/GenerateRandomString.js
@@ -0,0 +1,15 @@
+
+
+// Generate Random String of Given Length:
+
+
+function randomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+console.log(randomString(10)); // Example Output: "A1b2C3d4E5"
+
